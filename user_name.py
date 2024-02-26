a4236b5a4a262f9de81c87042365a573b4f760f1
Enoswafula <enoswafula98@gmail.com>
2024-02-26 19:34:17 +0300

    Added user_name.py

---
+++ user_name.py
@@ -0,0 +1,5 @@
+name=input("What is your name?")
+age=input("What is your age?")
+location=input("What si your location?")
+
+print("Hello {},you are {} years old and you live in {}.".format(name,age,location))
